f95efc193c7444404e21cd2b141bee6880c587c8
Rittichai Praiboon <58227587+Z-Sang@users.noreply.github.com>
2023-05-05 14:03:10 +0700

    add print_name

--- print_student_id.py
+++ print_student_id.py
@@ -6,9 +6,11 @@ def print_id(student_id):
 def calculate_sum(array):
     return array.sum()
 
-def ...
+def print_name(name):
+    print("Your name is {} ".format(name))
 
 
 if "__name__" == "__main__":
     print_id("345678")
     calculate_sum(np.array([1,2,3]))
+    print_name("Sang")
